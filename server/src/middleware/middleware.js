2ffefa07150a7377f9b46d04f75a6899836fe729
James Wotherspoon <james.t.wotherspoon@gmail.com>
2023-07-18 16:46:29 +0100

    Server - Cors Policy set in middleware, allowed client origin defined in .env

--- server/src/middleware/middleware.js
+++ server/src/middleware/middleware.js
@@ -8,8 +8,26 @@ app.use(express.json())
 
 // Middleware for setting headers
 app.use((req, res, next) => {
+  const allowedClientOrigin = process.env.CLIENT_ORIGIN
+  // Set the CORS headers to allow requests from the client origin
+  res.setHeader('Access-Control-Allow-Origin', allowedClientOrigin)
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
+  // Logging for non-specified origin requests
+  if (req.headers.origin && req.headers.origin !== allowedClientOrigin) {
+    logger.seriousLogger.warn(
+      `Non-specified origin attempted to make a request: ${req.headers.origin}`
+    )
+  }
+  next()
+})
+app.use((req, res, next) => {
   // Set the Content-Security-Policy header to only allow content recieved from this server
-  res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self'")
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; script-src 'self'"
+  )
   next()
 })
 
@@ -20,7 +38,9 @@ const limiter = rateLimit({
   max: 100, // Max amount per time span
   handler: (req, res, next) => {
     // If rate is exceeded respond with status code 429 an error message
-    res.status(429).json({ error: 'Too many requests, please try again later.' })
+    res
+      .status(429)
+      .json({ error: 'Too many requests, please try again later.' })
     // Log when the rate limit is reached
     logger.seriousLogger.warn(`Rate limit exceeded for IP ${req.ip}`)
   }
