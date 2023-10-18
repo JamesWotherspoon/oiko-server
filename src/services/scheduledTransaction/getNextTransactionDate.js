35e23a72682509589ea757e2731de4cc34d146e0
James Wotherspoon <james.t.wotherspoon@gmail.com>
2023-10-19 00:24:41 +0100

    Refactor: getNextTransactionDate

--- src/services/scheduledTransaction/getNextTransactionDate.js
+++ src/services/scheduledTransaction/getNextTransactionDate.js
@@ -3,36 +3,34 @@ function getNextTransactionDate(recurrenceType, dayOfWeek, dateOfMonth, monthOfY
   const nextDate = lastDate;
 
   switch (recurrenceType) {
-    case 'daily':
-      nextDate.setDate(lastDate.getDate() + 1);
-      break;
-    case 'weekly':
-      const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-      const dayIndex = daysOfWeek.indexOf(dayOfWeek);
-      const daysUntilNext = (dayIndex - lastDate.getDay() + 7) % 7;
-      nextDate.setDate(lastDate.getDate() + daysUntilNext);
-      break;
-    case 'monthly':
-      nextDate.setMonth(lastDate.getMonth() + 1);
-      nextDate.setDate(dateOfMonth);
-      break;
-    case 'quarterly':
-      nextDate.setMonth(lastDate.getMonth() + 3);
-      nextDate.setDate(dateOfMonth);
-      break;
-    case 'biannually':
-      console.log(lastDate + ' ' + nextDate);
-      nextDate.setMonth(lastDate.getMonth() + 6);
-      nextDate.setDate(dateOfMonth);
-      break;
-    case 'annually':
-      nextDate.setFullYear(lastDate.getFullYear() + 1);
-      nextDate.setMonth(monthOfYear);
-      console.log(dateOfMonth);
-      nextDate.setDate(15);
-      break;
-    default:
-      throw new Error('Invalid recurrence type');
+  case 'daily':
+    nextDate.setDate(lastDate.getDate() + 1);
+    break;
+  case 'weekly':
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const dayIndex = daysOfWeek.indexOf(dayOfWeek);
+    const daysUntilNext = (dayIndex - lastDate.getDay() + 7) % 7;
+    nextDate.setDate(lastDate.getDate() + daysUntilNext);
+    break;
+  case 'monthly':
+    nextDate.setMonth(lastDate.getMonth() + 1);
+    nextDate.setDate(dateOfMonth);
+    break;
+  case 'quarterly':
+    nextDate.setMonth(lastDate.getMonth() + 3);
+    nextDate.setDate(dateOfMonth);
+    break;
+  case 'biannually':
+    nextDate.setMonth(lastDate.getMonth() + 6);
+    nextDate.setDate(dateOfMonth);
+    break;
+  case 'annually':
+    nextDate.setFullYear(lastDate.getFullYear() + 1);
+    nextDate.setMonth(monthOfYear);
+    nextDate.setDate(15);
+    break;
+  default:
+    throw new Error('Invalid recurrence type');
   }
   nextDate.setUTCHours(4);
 
